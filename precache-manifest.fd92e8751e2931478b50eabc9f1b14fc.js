065b8b0f594b77f6ccdc7d0099ab9bc4acfc5314
dormenog <dormenog@gmail.com>
2018-07-13 10:16:33 +1000

    grammar fixes

--- precache-manifest.fd92e8751e2931478b50eabc9f1b14fc.js
+++ precache-manifest.fd92e8751e2931478b50eabc9f1b14fc.js
@@ -4,20 +4,24 @@ self.__precacheManifest = [
     "url": "/js/chunk-vendors.80825092.js"
   },
   {
-    "revision": "e19e28376406849680cf",
-    "url": "/js/app.e19e2837.js"
+    "revision": "3a44dcb861e66389cc21",
+    "url": "/js/app.3a44dcb8.js"
   },
   {
-    "revision": "45f60e2360361e6b89400cdb26355c63",
+    "revision": "4fc6593fbc0a66f721ae82830f25c0e0",
     "url": "/index.html"
+  },
+  {
+    "revision": "98860d795f7da2e1b4a2e567640de2ff",
+    "url": "/img/AFLogo.svg"
   },
   {
     "revision": "808250927e30cb8e8c99",
     "url": "/css/chunk-vendors.159878e2.css"
   },
   {
-    "revision": "e19e28376406849680cf",
-    "url": "/css/app.9945a443.css"
+    "revision": "3a44dcb861e66389cc21",
+    "url": "/css/app.404c7d11.css"
   },
   {
     "revision": "b484b785be5f8d1a7b5b90fe72f933bb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc2daecd96fb187062171e218d56e190.js"
+  "/precache-manifest.fd92e8751e2931478b50eabc9f1b14fc.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vuecv"});
